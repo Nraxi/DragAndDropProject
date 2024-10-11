2f83db384bcca013f8d78e8fa7ce7c5ba7ec3c03
nraxi <olle.code@hotmail.com>
2024-10-11 13:45:22 +0200

    ändra text

--- obj/Debug/net7.0/DragAndDropProject.AssemblyInfo.cs
+++ obj/Debug/net7.0/DragAndDropProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DragAndDropProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b23008d59a05025286c164fe5dd704acadef4b1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4f848510064c3351859cd6a3a836809ce438b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DragAndDropProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DragAndDropProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
