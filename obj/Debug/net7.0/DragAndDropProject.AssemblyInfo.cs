27be5f6c1278aad07aaeec54d6752564fdac4efa
nraxi <olle.code@hotmail.com>
2024-10-14 09:59:50 +0200

    a

--- obj/Debug/net7.0/DragAndDropProject.AssemblyInfo.cs
+++ obj/Debug/net7.0/DragAndDropProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DragAndDropProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b343a75894d2f8ec33a9b71f0adb59c2e51fa12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+708cd3fa85606bfe65d766ad921a9e4d38449b6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DragAndDropProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DragAndDropProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
